e65d18c2df7484c0d2b62c68faf67c957caa1602
Netanel Nadav <Natinadav932@gmail.com>
2021-12-29 15:43:49 +0200

    added mails

--- js/apps/mail/cmps/email-list.jsx
+++ js/apps/mail/cmps/email-list.jsx
@@ -5,8 +5,14 @@ export function EmailList({ emails }) {
     if (!emails.length) return <h1>No Mails</h1>
     return (
         <section className="emails-list main-layout">
-            {emails.map(email => <EmailPreview key={email.id} email={email} />)}
-
+            <table>
+                <tr>
+                    <th>to:</th>
+                    <th>Subject</th>
+                    <th>Time Published</th>
+                </tr>
+                    {emails.map(email => <EmailPreview key={email.id} email={email} />)}
+            </table>
         </section>
     )
 }
--- js/apps/mail/cmps/email-preview.jsx
+++ js/apps/mail/cmps/email-preview.jsx
@@ -2,12 +2,22 @@
 
 
 export function EmailPreview({ email }) {
-    const { id, body, sentAt, subject, to} = email
+    const { id, body, sentAt, subject, to } = email
     return (
-        <section className="email-preview flex align-center space-between">
-            <h1>{to}</h1>
-            <p>{subject}</p>
-            <p>{body}</p>
-        </section>
+        <tr>
+            <td>{to}</td>
+            <td>{body}</td>
+            <td>{sentAt}</td>
+        </tr>
     )
 }
+
+
+
+
+
+{/* <section className="email-preview flex align-center space-between">
+<h1>{to}</h1>
+<p>{subject}</p>
+<p>{body}</p>
+</section> */}
