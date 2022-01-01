9bd85061592c1446da2d578369a4f6b56f606093
Netanel Nadav <natinadav932@gmail.com>
2022-01-01 23:05:25 +0200

    final

--- js/apps/keep/cmps/note-bar.jsx
+++ js/apps/keep/cmps/note-bar.jsx
@@ -45,10 +45,10 @@ export class NoteBar extends React.Component {
     render() {
         const { placeHolder, txt } = this.state.input;
         return (
-            <section className="add-note flex">
-                <form onSumbit={this.onSaveNote}>
+            <section className="add-note flex justify-center align-center">
+                <form onSubmit={this.onSaveNote}>
                     <label htmlFor="note-input"></label>
-                    <input type="text" id="note-input" name="note-input" placeholder={placeHolder} value={txt} onChange={this.handleChange} />
+                    <input type="text" className="note-input" id="note-input" name="note-input" placeholder={placeHolder} value={txt} onChange={this.handleChange} />
                     <button className={`bar-btn far fa-comment`} onClick={() => this.onSetInput('note-txt')}></button>
                     <button className={`bar-btn far fa-image`} onClick={() => this.onSetInput('note-img')}></button>
                     <button className={`bar-btn fab fa-youtube`} onClick={() => this.onSetInput('note-video')}></button>
--- js/apps/mail/cmps/email-list.jsx
+++ js/apps/mail/cmps/email-list.jsx
@@ -2,22 +2,15 @@ import { EmailPreview } from './email-preview.jsx'
 
 
 export function EmailList({ emails, markReadEmail, onMoveToTrash, onExpandEmail, onStarredEmail }) {
-    if (!emails.length) return (
-        <div className="no-mails-warrper flex justify-center align-center">
-            <h1>No Mails to show</h1>
-        </div>
-
+    if (!emails.length) return  (
+    <div className="no-mails-warrper flex justify-center align-center">
+        <h1>No Mails to show</h1>
+    </div>
+    
     )
     return (
-        <section className="emails-list main-layout">
-            <table>
-                <tr>
-                    <th>to:</th>
-                    <th>Subject</th>
-                    <th>Time Published</th>
-                </tr>
-                {emails.map(email => <EmailPreview key={email.id} email={email} />)}
-            </table>
-        </section>
+        <section className="emails-list">
+            {emails.map(email => <EmailPreview key={email.id} email={email} markReadEmail={markReadEmail} onMoveToTrash={onMoveToTrash} onExpandEmail={onExpandEmail} onStarredEmail={onStarredEmail} />)}
+        </section> 
     )
 }
